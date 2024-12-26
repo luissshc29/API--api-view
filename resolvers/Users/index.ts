2737d1f79eda1589ed688fc659768848b7c3d8af
Luis Henrique <122834024+luissshc29@users.noreply.github.com>
2024-12-26 14:13:50 +0000

    Changed default value of indexRef to "0"

--- resolvers/Users/index.ts
+++ resolvers/Users/index.ts
@@ -61,7 +61,7 @@ const userResolvers = {
 
         // Gets the last indexRef in the users array;
         const usersArray = await prisma.users.findMany();
-        const lastIndexRef = usersArray.length > 0 ? usersArray[usersArray.length - 1].indexRef : 1;
+        const lastIndexRef = usersArray.length > 0 ? usersArray[usersArray.length - 1].indexRef : 0;
         const newUser = {
           ...data,
           indexRef: lastIndexRef + 1,
